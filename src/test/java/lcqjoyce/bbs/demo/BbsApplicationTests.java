e85d5605a5be8f6de5cc3e1497da820156d588b4
LCQJOYCE <1305941975@qq.com>
2020-04-11 15:24:44 +0800

    Initial commit

---
+++ src/test/java/lcqjoyce/bbs/demo/BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package lcqjoyce.bbs.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
